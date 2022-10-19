db27a187fd34cf59cd20d0bedb5de121ce2b9278
Shivanshu Nigam <shivanshunigam0201@gmail.com>
2022-10-19 12:26:00 +0530

    Changes related to login and env files

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css';
 document.addEventListener("DOMContentLoaded",()=>{
   //If user had already login into account then redirect to Dashboard else to login page
   if (localStorage.getItem("accessToken")){
-    window.location.href="dashoard/dashboard.html";
+    window.location.href="dashboard/dashboard.html";
   }else{
     window.location.href="login/login.html";
   }
