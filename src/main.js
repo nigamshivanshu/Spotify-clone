3f4a51ef19d7e1591e9bee9baeedb5ef7b3361c7
Shivanshu Nigam <shivanshunigam0201@gmail.com>
2022-11-15 14:02:50 +0530

    Some additional fuctionalities added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import './style.css';
+const APP_URL=import.meta.env.VITE_APP_URL;//for importing APP_URL from env file
 
 document.addEventListener("DOMContentLoaded",()=>{
   //If user had already login into account then redirect to Dashboard else to login page
   if (localStorage.getItem("accessToken")){
-    window.location.href="dashboard/dashboard.html";
+    window.location.href=`${APP_URL}/dashboard/dashboard.html`;
   }else{
-    window.location.href="login/login.html";
+    window.location.href=`${APP_URL}/login/login.html`;
   }
 })
